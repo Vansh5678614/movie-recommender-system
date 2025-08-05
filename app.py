b90e2cf0b7bd640589896cadeb424db158465ec3
Vansh Hari <vanshhari222@gmail.com>
2025-08-05 17:55:02 +0530

    Initial commit without large files

---
+++ app.py
@@ -0,0 +1,47 @@
+import pickle
+import streamlit as st
+import requests
+
+# ✅ Fetch movie poster from TMDB
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+    data = requests.get(url).json()
+    poster_path = data.get('poster_path')
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+# ✅ Recommend top 5 similar movies
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names, recommended_movie_posters
+
+# ✅ Streamlit UI
+st.header('Movie Recommender System')
+
+# If files are in root folder:
+movies = pickle.load(open('movie_list.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.text(recommended_movie_names[i])
+            st.image(recommended_movie_posters[i])
